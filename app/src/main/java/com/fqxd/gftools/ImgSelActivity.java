0a02132fa7253f3e69f44d149366cfe1f77e802c
choiman1559 <cuj1559@gmail.com>
2019-12-01 02:19:41 +0900

    fixed some decompressor source

---
+++ app/src/main/java/com/fqxd/gftools/ImgSelActivity.java
@@ -0,0 +1,13 @@
+package com.fqxd.gftools;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImgSelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_imgsel);
+    }
+}
