63b18eea7e3b7675fe48a0a117fb3061ec0f04d3
choiman1559 <cuj1559@gmail.com>
2020-01-26 19:08:37 +0900

    added quick setting tile

---
+++ app/src/main/java/com/fqxd/gftools/alarm/palarm/TileServiceClass.java
@@ -0,0 +1,39 @@
+package com.fqxd.gftools.alarm.palarm;
+
+import android.annotation.TargetApi;
+import android.service.quicksettings.TileService;
+
+import com.fqxd.gftools.vpn.utils.VpnServiceHelper;
+
+@TargetApi(24)
+public class TileServiceClass extends TileService{
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+        if(getSharedPreferences("ListAlarm",MODE_PRIVATE).getBoolean("isChecked",false) && !VpnServiceHelper.vpnRunningStatus()) getQsTile().setState(1);
+        else getQsTile().setState(2);
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onTileRemoved() {
+        super.onTileRemoved();
+    }
+
+    @Override
+    public void onClick() {
+        if(getQsTile().getState() == 1) {
+            PAlarmAddClass.context = TileServiceClass.this;
+            getSharedPreferences("ListAlarm",MODE_PRIVATE).edit().putBoolean("isChecked", true).apply();
+            getQsTile().setState(2);
+            getQsTile().updateTile();
+            new PacketClass().setVpn(TileServiceClass.this);
+            new PacketClass().runVpn(TileServiceClass.this);
+        } else {
+            getSharedPreferences("ListAlarm",MODE_PRIVATE).edit().putBoolean("isChecked", false).apply();
+            getQsTile().setState(1);
+            getQsTile().updateTile();
+            new PacketClass().endVpn(TileServiceClass.this);
+        }
+    }
+}
