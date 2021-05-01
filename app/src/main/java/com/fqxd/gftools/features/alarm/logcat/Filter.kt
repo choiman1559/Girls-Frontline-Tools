796992122016182d49b2b8f87c90e133f3fb4413
choiman1559 <cuj1559@gmail.com>
2021-05-02 05:32:29 +0900

    deleted unuse classes

--- app/src/main/java/com/fqxd/gftools/features/alarm/logcat/Filter.kt
+++
@@ -1,6 +0,0 @@
-package com.fqxd.gftools.features.alarm.logcat
-
-interface Filter {
-
-    fun apply(log: Log): Boolean
-}
--- app/src/main/java/com/fqxd/gftools/features/alarm/logcat/LogsReceivedListener.kt
+++
@@ -1,9 +0,0 @@
-package com.fqxd.gftools.features.alarm.logcat
-
-import androidx.annotation.MainThread
-
-interface LogsReceivedListener {
-
-    @MainThread
-    fun onReceivedLogs(logs: List<Log>)
-}
